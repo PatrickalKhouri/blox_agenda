1f4b877474308a28aec256e42305e8789c701e91
Patrick Khouri <patrick.darya@gmail.com>
2021-01-16 11:00:24 -0300

    Added validations to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 class Room < ApplicationRecord
   has_many :meetings
+  validates :name, :capacity, presence: true
+  validates :name, uniqueness: true
+  validates :capacity, :numericality => { greater_than: 0}
 end
