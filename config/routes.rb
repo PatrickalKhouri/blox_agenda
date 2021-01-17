4931f492505a51b49f6a4995d1bbb05e21312dac
Patrick Khouri <patrick.darya@gmail.com>
2021-01-17 13:53:17 -0300

    Added a calendar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'meetings#index'
 
   resources :rooms, only: %i[index new create destroy]
-  resources :meetings, only: %i[index new create destroy]
+  resources :meetings, only: %i[index show new create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
