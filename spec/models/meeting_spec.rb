d933b92ed4b011ce700e4e6b04e34061b0f24ca0
Patrick Khouri <patrick.darya@gmail.com>
2021-01-17 12:36:49 -0300

    figuring out the tests

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
   context "validation tests" do
+  
     it 'ensures date presence' do
-      meeting = Meeting.create( room: Room.first, user: User.first, date:"18/10/2020", start_time: "18:00", duration_minutes: 30)
-      meeting.room_id
+      meeting = Meeting.new(date:"18/10/2020", start_time: "18:00", duration_minutes: 30, title: "Test meeting").save
       expect(meeting).to eq(false) 
     end
 
@@ -29,7 +29,7 @@ RSpec.describe Meeting, type: :model do
     end
 
     it 'ensures two meetings cant be scheduled at the same room at the same duration' do
-      meeting1 = Meeting.create(room_id: 2, user_id: 3 PmPm, duration_minutes: 30, date: "10/12/2020", start_time:"19:00")
+      meeting1 = Meeting.create(room_id: 2, user_id: 3, duration_minutes: 30, date: "10/12/2020", start_time:"19:00")
       meeting2 = Meeting.new(room_id: 2, user_id: 2, duration_minutes: 30, date: "10/12/2020", start_time:"19:15").save
       expect(meeting2).to eq(false) 
     end
