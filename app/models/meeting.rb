31bc63747a700baf8395149dfd55e93e68c339ed
Patrick Khouri <patrick.darya@gmail.com>
2021-01-16 21:18:20 -0300

    Started doing the restrictions, need to do better seeds

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,4 +3,14 @@ class Meeting < ApplicationRecord
   belongs_to :user
   validates :date, :start_time, :duration_minutes, :title, presence: true
   validates :duration_minutes, :numericality => { greater_than_or_equal_to: 15}
+  validate :meeting_conflicts, :before => :create
+
+  private
+
+  def meeting_conflicts
+  end
+
+  def end_time
+    end_time = self.start_time + duration_minutes
+  end
 end
